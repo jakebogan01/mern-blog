292a96874bceb6be52274d8ef759f17799deba3a
jakebogan01 <jakebogan01@gmail.com>
2024-03-25 12:04:39 -0400

    creates signup api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,3 @@
 export const test = (req, res) => {
-        res.send("Hello World!")
-}
+        res.send("Hello World!");
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
         username: {
@@ -20,8 +20,8 @@ const userSchema = new mongoose.Schema({
                 min: 6,
                 max: 225
         }
-}, {timestamps: true})
+}, {timestamps: true});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
