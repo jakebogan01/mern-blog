8c20c4613b7dccf4733e78e702733e38e53059ee
jakebogan01 <jakebogan01@gmail.com>
2024-03-30 21:05:06 -0400

    adds admin to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
                 type: String,
                 default: "https://unavatar.io/github/eugenio.oliveira"
         },
+        adminType: {
+                type: String,
+                default: "member",
+        },
 }, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
